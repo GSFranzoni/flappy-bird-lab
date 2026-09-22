fafff092f439035762e466cc303ba1a1f0ef4088
Guilherme Franzoni <50160004+GSFranzoni@users.noreply.github.com>
2026-09-22 12:59:07 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <span className="text-purple-500">React Vite Template</span>;
+}
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import path from "node:path";
+
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const getBase = () => {
+  const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+
+  if (process.env.VITE_BASE_PATH) {
+    return `${process.env.VITE_BASE_PATH.replace(/\/+$/, "")}/`;
+  }
+
+  if (process.env.GITHUB_ACTIONS && repositoryName) {
+    return `/${repositoryName}/`;
+  }
+
+  return "/";
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: getBase(),
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@/": path.resolve(import.meta.dirname, "src"),
+    },
+  },
+});
